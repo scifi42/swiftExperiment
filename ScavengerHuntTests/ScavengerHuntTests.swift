88981e1de4011f4c5b9bfbb8666ce33a0311ec80
Apple <apple@apples-macbook-58.local>
2016-03-05 06:42:42 -0800

    Initial Commit

---
+++ ScavengerHuntTests/ScavengerHuntTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScavengerHuntTests.swift
+//  ScavengerHuntTests
+//
+//  Created by Apple on 3/5/16.
+//  Copyright © 2016 Apple. All rights reserved.
+//
+
+import XCTest
+@testable import ScavengerHunt
+
+class ScavengerHuntTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
